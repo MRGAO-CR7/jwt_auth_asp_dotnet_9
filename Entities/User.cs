d5dfd97da57dcfe8fd88f21f80564d74e908686e
ERIC JIAN GAO <eric.gao.nz@gmail.com>
2025-06-13 14:03:12 +1200

    first commit

---
+++ Entities/User.cs
@@ -0,0 +1,9 @@
+namespace JwtAuthDotNet9.Entities
+{
+    public class User
+    {
+        public string UserName { get; set; } = string.Empty;
+
+        public string PasswordHash { get; set; } = string.Empty;
+    }
+}
